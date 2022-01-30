4f0fed470a515d2cdfa7a71d994e94e2e9e1130a
rm-ryou <rmco.420118@gmail.com>
2022-01-30 19:13:02 +0900

    fix substr

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmoriya   <rmoriya@student.42tokyo.jp      +#+  +:+       +#+        */
+/*   By: rmoriya <rmoriya@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:23:45 by rmoriya           #+#    #+#             */
-/*   Updated: 2022/01/28 23:23:45 by rmoriya          ###   ########.fr       */
+/*   Updated: 2022/01/30 19:12:18 by rmoriya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		len = 0;
+		return (ft_strdup(""));
 	else if ((start + len) > s_len)
 		len = s_len - start;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	if (start >= s_len)
-		return (str);
 	while (s[start + i] != '\0' && i < len)
 	{
 		str[i] = s[start + i];
