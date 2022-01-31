c616e79f6d6d3e111fbd442e2da353aa457ed41c
rm-ryou <rmco.420118@gmail.com>
2022-01-31 22:32:56 +0900

    add

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: rmoriya <rmoriya@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:23:45 by rmoriya           #+#    #+#             */
-/*   Updated: 2022/01/29 14:16:29 by rmoriya          ###   ########.fr       */
+/*   Updated: 2022/01/31 22:29:29 by rmoriya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strdup(const char *s1)
 	size_t	i;
 
 	i = 0;
+	if (!s1)
+		return (NULL);
 	ans = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!ans)
 		return (0);
