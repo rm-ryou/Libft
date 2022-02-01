0dddfed667ce70b0a0c954bf38d7e6d62ccef668
rm-ryou <rmco.420118@gmail.com>
2022-02-01 15:45:06 +0900

    fix strjoin.c

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rmoriya <rmoriya@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:23:45 by rmoriya           #+#    #+#             */
-/*   Updated: 2022/02/01 15:27:08 by rmoriya          ###   ########.fr       */
+/*   Updated: 2022/02/01 15:44:34 by rmoriya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1)
 		return (NULL);
-	s1_len = ft_strlen(s1);
 	if (!s2)
-		s2_len = 0;
-	else
-		s2_len = ft_strlen(s2);
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
